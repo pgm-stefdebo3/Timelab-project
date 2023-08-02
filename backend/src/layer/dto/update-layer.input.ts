444b0be942f44d682fb5d8bb7feea513f4b149c3
De Boeck Stef <91065663+pgm-stefdebo3@users.noreply.github.com>
2023-08-02 18:48:47 +0200

    Revert "Fully implement import"

--- backend/src/layer/dto/update-layer.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateLayerInput } from './create-layer.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateLayerInput extends PartialType(CreateLayerInput) {
-  @Field(() => Int)
-  id: number;
-}
